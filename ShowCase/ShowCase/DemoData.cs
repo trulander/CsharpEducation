8b83d5d9ed3709316d5f4f5ae7ec350276c78a8b
Alexander <trulander@mail.ru>
2021-02-18 21:25:45 +0500

    Develop step

---
+++ ShowCase/ShowCase/DemoData.cs
@@ -0,0 +1,43 @@
+﻿using ShowCase.Controllers;
+using ShowCase.Models;
+
+namespace ShowCase
+{
+    public class DemoData
+    {
+        public DemoData(ModelController modelController, DataBase dataBase)
+        {
+            modelController.Create(new Shop<Case<Product<int>>>(4));
+            modelController.Create(dataBase.Shops[0], new Case<Product<int>>(3));
+            modelController.Create(dataBase.Shops[0].Storage[0], new Product<int>(2));
+            modelController.Create(dataBase.Shops[0].Storage[0], new Product<int>(2));
+            
+            modelController.Create(dataBase.Shops[0], new Case<Product<int>>(2));
+            modelController.Create(dataBase.Shops[0].Storage[1], new Product<int>(2));
+            modelController.Create(dataBase.Shops[0].Storage[1], new Product<int>(2));     
+            
+            modelController.Create(dataBase.Shops[0], new Case<Product<int>>(2));
+            modelController.Create(dataBase.Shops[0].Storage[2], new Product<int>(2));
+            modelController.Create(dataBase.Shops[0].Storage[2], new Product<int>(2));     
+            
+            modelController.Create(dataBase.Shops[0], new Case<Product<int>>(2));
+            modelController.Create(dataBase.Shops[0].Storage[3], new Product<int>(2));
+            modelController.Create(dataBase.Shops[0].Storage[3], new Product<int>(2)); 
+            
+            modelController.Create(new Shop<Case<Product<int>>>(5));
+            modelController.Create(dataBase.Shops[1], new Case<Product<int>>(3));
+            modelController.Create(dataBase.Shops[1].Storage[0], new Product<int>(2));
+            modelController.Create(dataBase.Shops[1].Storage[0], new Product<int>(2));
+            
+            modelController.Create(dataBase.Shops[1], new Case<Product<int>>(2));
+            modelController.Create(dataBase.Shops[1].Storage[1], new Product<int>(2));
+            modelController.Create(dataBase.Shops[1].Storage[1], new Product<int>(2));     
+            
+            modelController.Create(dataBase.Shops[1], new Case<Product<int>>(2));
+            modelController.Create(dataBase.Shops[1].Storage[2], new Product<int>(2));
+            modelController.Create(dataBase.Shops[1].Storage[2], new Product<int>(2));     
+            
+
+        }
+    }
+}
